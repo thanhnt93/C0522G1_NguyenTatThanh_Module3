892575fca89d5f578df04ceafcc4d9f6c0ddf3d8
thanhnt93 <thanhnguyentat24122000@gmail.com>
2022-08-08 08:38:21 +0700

    update assignment 1

---
+++ ss1_cosodulieuquanhe/student-management.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `class` (
+  `id` int NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `teacher` (
+  `id` int NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `age` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
