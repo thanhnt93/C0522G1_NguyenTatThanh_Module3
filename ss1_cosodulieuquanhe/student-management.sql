99575c117d59e12fe0f1acdb2db7f58f1b06efe3
thanhnt93 <thanhnguyentat24122000@gmail.com>
2022-08-08 09:18:01 +0700

    update assignemt

--- ss1_cosodulieuquanhe/student-management.sql
+++ ss1_cosodulieuquanhe/student-management.sql
@@ -1,15 +1,34 @@
-CREATE TABLE `class` (
-  `id` int NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE SCHEMA `student-management`;
+
+CREATE TABLE `student-management`.class (
+  id int NOT NULL,
+  name varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
 
 
-CREATE TABLE `teacher` (
-  `id` int NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `age` varchar(45) DEFAULT NULL,
-  `country` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `student-management`.teacher (
+  id int NOT NULL,
+  name varchar(45) DEFAULT NULL,
+  age varchar(45) DEFAULT NULL,
+  country varchar(45) DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO `student-management`.class (id,name) VALUES (1, 'Nguyễn Tất Thành');
+INSERT INTO `student-management`.class (id,name) VALUES (2, 'Bùi Hùng');
+INSERT INTO `student-management`.class (id,name) VALUES (3, 'Hà Bảo Ân');
+INSERT INTO `student-management`.class (id,name) VALUES (4, 'Nguyễn Tấn Huân');
+INSERT INTO `student-management`.class (id,name) VALUES (5, 'Hồ Hải Học');
+
+INSERT INTO `student-management`.teacher (id, name, age, country) VALUES (1, 'Cong NT', '10', 'Đà Nẵng');
+INSERT INTO `student-management`.teacher (id, name, age, country) VALUES (2, 'Quang NN', '10', 'Đà Nẵng ');
+INSERT INTO `student-management`.teacher (id, name, age, country) VALUES (3, 'Chanh TV', '10', 'Đà Nẵng');
+INSERT INTO `student-management`.teacher (id, name, age, country) VALUES (4, 'Trung DC', '10', 'Đà Nẵng');
+INSERT INTO `student-management`.teacher (id, name, age, country) VALUES (5, 'Hai TT', '10', 'Đà Nẵng');
+
+
+
+
+
 
