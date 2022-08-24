c6ef04d8400de7b24b0a60da36be5bd5a4110a84
thanhnt93 <thanhnguyentat24122000@gmail.com>
2022-08-24 15:48:44 +0700

    update assignment

--- ss10_jsp_jslt/exercise/caculator/src/main/java/CalculatorServlet.java
+++ ss10_jsp_jslt/exercise/caculator/src/main/java/CalculatorServlet.java
@@ -1,42 +1,44 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name = "CalculatorServlet", urlPatterns = "/calculate")
+@WebServlet(name = "CalculatorServlet", urlPatterns = "/calculator")
 public class CalculatorServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
-        float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float firstOperand = Float.parseFloat(request.getParameter("firstOperand"));
+        float secondOperand = Float.parseFloat(request.getParameter("secondOperand"));
         char operator = request.getParameter("operator").charAt(0);
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        writer.print("<p>Result: </p>");
-        try{
-            float result = Calculator.calculate(firstOperand, secondOperand, operator);
-            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
-        }catch (Exception ex){
-            writer.println("Error: " + ex.getMessage());
+        String result;
+        switch (operator) {
+            case '+':
+                result = String.valueOf(firstOperand + secondOperand);
+                break;
+            case '-':
+                result = String.valueOf(firstOperand - secondOperand);
+                break;
+            case '*':
+                result = String.valueOf(firstOperand * secondOperand);
+                break;
+            default:
+                if (secondOperand == 0) {
+                    result = "Can't divide by zero";
+                } else {
+                    result = String.valueOf(firstOperand / secondOperand);
+                }
         }
-        writer.println("</html>");
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("display.jsp");
+        request.setAttribute("result", result);
+        request.setAttribute("firstOperand", firstOperand);
+        request.setAttribute("secondOperand", secondOperand);
+        request.setAttribute("operator", operator);
+        requestDispatcher.forward(request, response);
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        float firstOperand = Integer.parseInt(request.getParameter("first-operand"));
-        float secondOperand = Integer.parseInt(request.getParameter("second-operand"));
-        char operator = request.getParameter("operator").charAt(0);
-        PrintWriter writer = response.getWriter();
-        writer.println("<html>");
-        writer.print("<p>Result: </p>");
-        try{
-            float result = Calculator.calculate(firstOperand, secondOperand, operator);
-            writer.println(firstOperand + " " + operator + " " + secondOperand + " = " + result);
-        }catch (Exception ex){
-            writer.println("Error: " + ex.getMessage());
-        }
-        writer.println("</html>");
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
--- ss10_jsp_jslt/exercise/caculator/src/main/webapp/index.jsp
+++ ss10_jsp_jslt/exercise/caculator/src/main/webapp/index.jsp
@@ -1,40 +1,40 @@
 <%--
   Created by IntelliJ IDEA.
-  User: ADMIN
+  User: ASUS
   Date: 8/24/2022
-  Time: 2:10 PM
+  Time: 1:39 PM
   To change this template use File | Settings | File Templates.
 --%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
   <title>Calculator</title>
 </head>
 <body>
 <h1>Simple Calculator</h1>
-<form method="get" action="/calculate">
+<form method="get" action="/calculator">
   <fieldset>
     <legend>Calculator</legend>
     <table>
       <tr>
-        <td>First operand: </td>
-        <td><input name="first-operand" type="text"/></td>
+        <td>First operand:</td>
+        <td><input name="firstOperand" type="text"/></td>
       </tr>
       <tr>
-        <td>Operator: </td>
+        <td>Operator:</td>
         <td>
           <select name="operator">
-            <option value="+">Addition (+)</option>
-            <option value="-">Subtraction (-)</option>
-            <option value="*">Multiplication (*)</option>
-            <option value="/">Division (/)</option>
+            <option value="+">Addition</option>
+            <option value="-">Subtraction</option>
+            <option value="*">Multiplication</option>
+            <option value="/">Division</option>
           </select>
         </td>
       </tr>
       <tr>
-        <td>Second operand: </td>
-        <td><input name="second-operand" type="text"/></td>
+        <td>Second operand:</td>
+        <td><input name="secondOperand" type="text"/></td>
       </tr>
       <tr>
         <td></td>
@@ -45,4 +45,3 @@
 </form>
 </body>
 </html>
-
